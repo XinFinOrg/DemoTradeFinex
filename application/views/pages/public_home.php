48fd8ac0ed0df415556ee326198d6d3722f93b7a
Ritesh kakkad <44856884+riteshkakkad@users.noreply.github.com>
2019-03-10 22:00:53 +0530

    Update public_home.php

--- application/views/pages/public_home.php
+++ application/views/pages/public_home.php
@@ -260,7 +260,7 @@
 				<div class="icon col-xs-4 col-md-12 col-sm-4"> <img src="<?php echo base_url() ?>assets/images/icon/suppliers_icon.svg" alt="icon"> </div>
 				<div class="col-xs-8 col-md-12 col-sm-8">
 					<h3>R3 Corda Bridge</h3>
-					<p>Connect Corda State with Public blockchain. Mitigate Counter-Party Risk by making your Financial Instruments Interoperable & Liquid (Coming Soon)</p>
+					<p>Connect R3 Corda State with Public blockchain. Mitigate Counter-Party Risk by making your Financial Instruments Interoperable & Liquid (Coming Soon)</p>
 					<a href="<?=base_url('publicv/supplier');?>">Explore now <span><img src="<?php echo base_url() ?>assets/images/icon/arrow_icon.jpg" alt="icon"> </span></a> 
 				</div>	
 			</div>
