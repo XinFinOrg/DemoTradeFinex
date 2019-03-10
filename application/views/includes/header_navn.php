215eb929fb7c8b25a6ee01e28eac59f7299adb70
Ritesh kakkad <44856884+riteshkakkad@users.noreply.github.com>
2019-03-10 22:18:42 +0530

    Update header_navn.php

--- application/views/includes/header_navn.php
+++ application/views/includes/header_navn.php
@@ -57,9 +57,11 @@
 				
 						<li class="hassubs"> <a href="javascript:void(0)" title="">Financial Instruments Supported</a>
 							<ul class="submenu">
-							    <li class=""><a href="<?=base_url();?>publicv/bond_create" title="Bond Creation">Bond</a></li>
-                                <li class=""><a href="<?=base_url();?>publicv/infactor" title="Know about infactor">Invoice</a></li>
-                                <!--<ul class="dropdown">
+							<li class=""><a href="<?=base_url();?>publicv/bond_create" title="Bond Creation">Bond</a></li>
+                                			<li class=""><a href="<?=base_url();?>publicv/infactor" title="Know about infactor">Invoice</a></li>
+								<li class=""><a href="<?=base_url();?>publicv/infactor" title="Know about infactor">Corda Bridge (Coming Soon)</a></li>
+                                				
+								<!--<ul class="dropdown">
 									<li class="subs"><a href="<?=base_url();?>publicv/infactor" title="Know about infactor">InFactor</a></li>
 									<li><a href="<?=base_url();?>publicv/financier" title="Know about financier">Financier</a></li>
 									<li><a href="<?=base_url();?>publicv/beneficiary" title="Know about beneficiary">Beneficiary</a></li>
