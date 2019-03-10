5e6ffc37c8094d4f3c6408a077b6be168b686d0a
Ritesh kakkad <44856884+riteshkakkad@users.noreply.github.com>
2019-03-10 21:04:33 +0530

    Update public_home.php

--- application/views/pages/public_home.php
+++ application/views/pages/public_home.php
@@ -244,7 +244,7 @@
 				<div class="icon col-xs-4 col-md-12 col-sm-4"> <img src="<?php echo base_url() ?>assets/images/icon/financier_icon.svg" alt="icon"> </div>
 				<div class="col-xs-8 col-md-12 col-sm-8">
 					<h3>Bond</h3>
-					<p>Create, Manage & Trade Bonds Online | A fully digital solution on blockchain </p>
+					<p>Create, Manage & Liquidate Bonds Online | A fully digital solution on blockchain </p>
 					<a href="<?=base_url('publicv/financier');?>">Explore now <span><img src="<?php echo base_url() ?>assets/images/icon/arrow_icon.jpg" alt="icon"> </span></a> 
 				</div>	
 			</div>
@@ -252,7 +252,7 @@
 				<div class="icon col-xs-4 col-md-12 col-sm-4"> <img src="<?php echo base_url() ?>assets/images/icon/beneficiary_icon.svg" alt="icon"> </div>
 				<div class="col-xs-8 col-md-12 col-sm-8">
 					<h3>Invoice</h3>
-					<p>InFactor is a platform for factoring Trade Instruments, mainly invoices</p>
+					<p>InFactor is a platform for factoring Trade Instruments(mainly invoices)</p>
 					<a href="<?=base_url('publicv/beneficiary');?>">Explore now <span><img src="<?php echo base_url() ?>assets/images/icon/arrow_icon.jpg" alt="icon"> </span></a> 
 				</div>	
 			</div>
