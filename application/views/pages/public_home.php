12961154cd3a8faf2b3a0271382781064c8eb428
Ritesh kakkad <44856884+riteshkakkad@users.noreply.github.com>
2019-03-13 12:42:47 +0530

    Update public_home.php

--- application/views/pages/public_home.php
+++ application/views/pages/public_home.php
@@ -23,11 +23,11 @@
 			</div> 
 			<div class="item"> <img src="<?=base_url();?>assets/images/slider/tradefinex_slider_3.jpg"/>
 				<div class="carousel-caption">
-					<div class="page-title-heading">Create Bridge & Gain access to global Financial ecosystem.</div>
+					<div class="page-title-heading">TradeFinex Round table series Consortium Event</div>
 					<div class="text-heading"> </div>
-					<p class="slider_lower_content">Start trial under regulatory sandbox environment.</p>
-					<a class="trade_home_call_to-action" href="<?=base_url('publicv/bond_create');?>">Start POC</a>
-					<a class="trade_home_call_to-action" href="https://docs.tradefinex.org/faq/general">Explore More</a>
+					<p class="slider_lower_content">one-day roundtable event that brings together trade and infrastructure finance stakeholders on one single platform to address the increasing global trade finance gap as well as infrastructure deficit by leveraging blockchain technology.</p>
+					<a class="trade_home_call_to-action" href="http://events.tradefinex.org/#contact">Start POC</a>
+					<a class="trade_home_call_to-action" href="http://events.tradefinex.org/">Explore More</a>
 				</div>
 			</div> 
 		</div>
