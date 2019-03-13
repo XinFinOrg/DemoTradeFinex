4df700d7c5a54970a66427d0ef29bbe243b272d7
Ritesh kakkad <44856884+riteshkakkad@users.noreply.github.com>
2019-03-13 07:25:48 +0530

    Update footer_commonn.php

--- application/views/includes/footer_commonn.php
+++ application/views/includes/footer_commonn.php
@@ -107,7 +107,8 @@
 									<li> <a href="<?=base_url();?>publicv/infactor" title="">Invoice</a></li>	
 								
 									<li> <a href="http://events.tradefinex.org/" title="">Consortium Event</a></li>
-                                    					<!--<li> <a href="<?=base_url();?>" title="">Start POC</a></li>-->
+                                    					<li> <a href="https://xinfinorg.github.io/TradeFinex_API/" title="">API</a></li>
+									<!--<li> <a href="<?=base_url();?>" title="">Start POC</a></li>-->
 								</ul>
 							</div>
 							<!-- /.widget-categories --> 
@@ -117,7 +118,7 @@
 							<div class="widget widget-categories">
 								<h3 class="widget-title">Others</h3>
 								<ul class="one-half">
-							<li> <a href="https://xinfinorg.github.io/TradeFinex_API/" title="">API</a></li>
+							
                                 			<li> <a href="<?=base_url();?>publicv/media_center" title="">Media Center</a></li>
 							<li> <a href="<?=base_url();?>publicv/faq" title="">FAQ's</a></li>
 							<li> <a href="<?=base_url();?>publicv/contact" title="">Contact Us</a></li>
