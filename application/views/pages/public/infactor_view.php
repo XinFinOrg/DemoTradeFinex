46a11eb0f58cce78714c9d5d0d19e59107763f08
Ritesh kakkad <44856884+riteshkakkad@users.noreply.github.com>
2019-03-15 08:37:14 +0530

    Update infactor_view.php

--- application/views/pages/public/infactor_view.php
+++ application/views/pages/public/infactor_view.php
@@ -46,7 +46,7 @@
                         <div class="inner">
                             <figure><img src="../assets/banner-assets/images/scalable-link-ico.png" class="img-responsive" alt=""></figure>
                             <h3>Connect using easy API Access</h3>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever</p>
+                            <p>Easy Integration with in a Click. Easy plugin available for Quickbooks user</p>
                             <a href="#" class="know-more">Know More</a> </div>
                     </li>
                 </ul>
