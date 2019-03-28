de39feb8244cedca42c6bfb2693c158b5136c236
Dattatray <dattatrayh@gmail.com>
2019-03-28 15:07:34 +0530

    Updates consortium_view.php
    bxslider.css changes

--- application/views/pages/public/consortium_view.php
+++ application/views/pages/public/consortium_view.php
@@ -1,133 +1,93 @@
 <div class="sub_page_wraper">
-	<section class="trade_career">
-		<div class="container  text-center">
-			<h3>Consortium Membership for Tradefinex</h3>
-			<!--<p>Consortium-Led Asset Tokenisation Platform Using Blockchain Technology</p>-->
-			<div class="btn-more">
-				<a class="play-btn video" href="https://www.youtube.com/watch?v=mjrhTnZ16f8"><span class="fa fa-play-circle"></span> Watch Video</a>
-                <!--<a href="https://youtu.be/mjrhTnZ16f8"><span> <i class="fa fa-play-circle"></i> </span> <span>Watch Video</span></a>-->
-			</div>
-		</div>
-	</section>
-    
-    
-    <section class="white-bg padding-lg">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="cnt-block padd-left">
-                            <h2 class="mb-15">Let's start the journey together.</h2>
-                            <p class="mb-15">You can participate in this consortium by joining as either a founding member or extended member of the consortium. As a member, you will be part of the ecosystem and will contribute to the evolution of TradeFinex platform. Your contribution in terms of best practices, driving the standardization and working with regulators will help you to influence the future of your industry. A consortium-led governance makes the platform truly decentralized which is in sync with the core philosophy of blockchain.</p>
-                            <p>Participate, co-develop and share in the following benefits of the TradeFinex blockchain powered platform membership:</p>
-                        </div>
-                    </div>
-                </div>
+    <section class="trade_career">
+        <div class="container  text-center">
+            <h3>Consortium Membership for Tradefinex</h3>
+            <!--<p>Consortium-Led Asset Tokenisation Platform Using Blockchain Technology</p>-->
+            <div class="btn-more">
+                <a class="play-btn video" href="https://www.youtube.com/watch?v=mjrhTnZ16f8"><span class="fa fa-play-circle"></span> Watch Video</a>
             </div>
-		</section>
-        
-        
-        
-	<section class="consortium-sec grey-bg padding-lg">
-            <div class="container">
-                <ul class="row consortium opt2">
-                    <li class="col-md-6">
-                        <div class="text-area">
-                            <h6>What is TradeFinex Consortium?</h6>
-                            <p>A forum made up of individuals, companies, organizations and governments to contribute towards the evolution of cross border trade and finance practise (using innovative technology) under Regulatory Framework.</p>
-                        </div>
-                    </li>
-                    <li class="col-md-6">
-                        <div class="text-area">
-                            <h6>Who can join the Consortium?</h6>
-                            <p>Our community includes Custodians, Banks, Broker/dealers, Corporates, Exchanges, Clearing houses, Remittance Company, securities and treasury market infrastructures etc.</p>
-                        </div>
-                    </li>
-                    <li class="col-md-6">
-                        <div class="text-area">
-                            <h6>How does this consortium operate?</h6>
-                            <p>consortium Manage and Govern by the Members only, However the entire programme will be Administered, monitored and maintained by TradeFinex Tech Pvt Ltd.</p>
-                        </div>
-                    </li>
-                    <li class="col-md-6">
-                        <div class="text-area">
-                            <h6>Whom do I contact for more information on TradeFinex Consortium?</h6>
-                            <p>Most of the information available on tradefinex website but if you still have question then feel free to contact at: https://demo.tradefinex.org/publicv/contact</p>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </section>
-        
-        
-        
-        
-    <div class="wrapper">
-    <div class="image-block-wrapper">
-      <div class="image-block col-md-6">
-        <div class="image-block-bg bg1"></div>
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 col-md-offset-6 mt-70 mb-70">
-            <div class="padd-left">
-            <h2>What your Membership includes</h2>
-			<ul class="more-features-list">
-				<li>Get support and interaction (Slack) with Consortium Colleagues.</li>
-				<li>Conduct Unlimited online Proof of Concepts with TradeFinex Platform.</li>
-				<li>Get Regular newsletters on activity in Trade and Finance.</li>
-				<li>Get Free virtual Pass (Sky Business Meeting) access to yearly Roundtable of TradeFinex. (Normal cost USD 200/per event access)</li>
-				<li>Get to interact with Trade Finance Ecosystem participants around the world for growing your business.</li>
-				<li>Your company logo and link in TradeFinex ecosystem partners.</li>
-				<li>A PoC completion press release to create awareness for your company.</li>
-				<li>Become Member in just USD 199. offer for a limited time only.</li>
-			</ul>
-            <!--<div class="more-features-btn-holder mt-20"><a href="#" class="get-started">Subscribe </a></div>-->
-            
-            <div class="more-features-btn-holder mt-20"><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input name="cmd" type="hidden" value="_s-xclick" /> <input name="hosted_button_id" type="hidden" value="XPXLS3L3KDGBJ" /> <input alt="PayPal &ndash; The safer, easier way to pay online!" name="submit" src="https://www.paypalobjects.com/en_GB/SG/i/btn/btn_subscribeCC_LG.gif" type="image" /> <img src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form></div>            
-            
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  </div>
-        
-        
-        
-        
-        <!--<section class="more-features grey-bg padding-lg">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 col-md-6">
-                        <h2>What your Membership includes</h2>
-                        <ul class="more-features-list">
-                            <li>Get support and interaction (Slack) with Consortium Colleagues.</li>
-                            <li>Conduct Unlimited online Proof of Concepts with TradeFinex Platform.</li>
-                            <li>Get Regular newsletters on activity in Trade and Finance.</li>
-							<li>Get Free virtual Pass (Sky Business Meeting) access to yearly Roundtable of TradeFinex. (Normal cost USD 200/per event access)</li>
-							<li>Get to interact with Trade Finance Ecosystem participants around the world for growing your business.</li>
-							<li>Your company logo and link in TradeFinex ecosystem partners.</li>
-							<li>A PoC completion press release to create awareness for your company.</li>
-							<li>Become Member in just USD 199. offer for a limited time only.</li>
-                        </ul>
-                        <div class="more-features-btn-holder mt-20"><a href="#" class="get-started">Subscribe </a></div>
-                    </div>
-                    <div class="col-sm-6 col-md-6 mt-80">
-                        <div class="videos_video_wrap">
-                    		<div class="row">
-                        		<div class="col-md-12 videos_video_wraper">
-                            		<iframe src="https://www.youtube.com/embed/mjrhTnZ16f8?feature=oembed" gesture="media" allowfullscreen="" width="100%" height="352" frameborder="0"></iframe>
-                        		</div>
-                    		</div>
-                		</div>
+    </section>
+
+    <section class="white-bg padding-lg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="cnt-block padd-left">
+                        <h2 class="mb-15">Let's start the journey together.</h2>
+                        <p class="mb-15">You can participate in this consortium by joining as either a founding member or extended member of the consortium. As a member, you will be part of the ecosystem and will contribute to the evolution of TradeFinex platform. Your contribution in terms of best practices, driving the standardization and working with regulators will help you to influence the future of your industry. A consortium-led governance makes the platform truly decentralized which is in sync with the core philosophy of blockchain.</p>
+                        <p>Participate, co-develop and share in the following benefits of the TradeFinex blockchain powered platform membership:</p>
                     </div>
                 </div>
             </div>
-        </section>-->
-        
-        
-        
-        
+        </div>
+    </section>
+
+    <div class="wrapper grey-bg">
+        <div class="image-block-wrapper">
+            <div class="image-block col-md-6">
+                <div class="image-block-bg bg1"></div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-6 mt-70 mb-70">
+                        <div class="padd-left">
+                            <h2>What your Membership includes</h2>
+                            <ul class="more-features-list">
+                                <li>Get support and interaction (Slack) with Consortium Colleagues.</li>
+                                <li>Conduct Unlimited online Proof of Concepts with TradeFinex Platform.</li>
+                                <li>Get Regular newsletters on activity in Trade and Finance.</li>
+                                <li>Get Free virtual Pass (Sky Business Meeting) access to yearly Roundtable of TradeFinex. (Normal cost USD 200/per event access)</li>
+                                <li>Get to interact with Trade Finance Ecosystem participants around the world for growing your business.</li>
+                                <li>Your company logo and link in TradeFinex ecosystem partners.</li>
+                                <li>A PoC completion press release to create awareness for your company.</li>
+                                <li>Become Member in just USD 199. offer for a limited time only.</li>
+                            </ul>
+                            <!--<div class="more-features-btn-holder mt-20"><a href="#" class="get-started">Subscribe </a></div>-->
+                            <div class="more-features-btn-holder mt-20">
+                                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                    <input name="cmd" type="hidden" value="_s-xclick" />
+                                    <input name="hosted_button_id" type="hidden" value="XPXLS3L3KDGBJ" />
+                                    <input alt="PayPal &ndash; The safer, easier way to pay online!" name="submit" src="https://www.paypalobjects.com/en_GB/SG/i/btn/btn_subscribeCC_LG.gif" type="image" /> <img src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="consortium-sec padding-lg">
+        <div class="container">
+            <ul class="row consortium opt2">
+                <li class="col-md-6">
+                    <div class="text-area">
+                        <h6>What is TradeFinex Consortium?</h6>
+                        <p>A forum made up of individuals, companies, organizations and governments to contribute towards the evolution of cross border trade and finance practise (using innovative technology) under Regulatory Framework.</p>
+                    </div>
+                </li>
+                <li class="col-md-6">
+                    <div class="text-area">
+                        <h6>Who can join the Consortium?</h6>
+                        <p>Our community includes Custodians, Banks, Broker/dealers, Corporates, Exchanges, Clearing houses, Remittance Company, securities and treasury market infrastructures etc.</p>
+                    </div>
+                </li>
+                <li class="col-md-6">
+                    <div class="text-area">
+                        <h6>How does this consortium operate?</h6>
+                        <p>consortium Manage and Govern by the Members only, However the entire programme will be Administered, monitored and maintained by TradeFinex Tech Pvt Ltd.</p>
+                    </div>
+                </li>
+                <li class="col-md-6">
+                    <div class="text-area">
+                        <h6>Whom do I contact for more information on TradeFinex Consortium?</h6>
+                        <p>Most of the information available on tradefinex website but if you still have question then feel free to contact at: https://demo.tradefinex.org/publicv/contact</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+
 </div>
 <?php
 	//$this->load->view('includes/block_features');
