a1d6061ed250e38ce66a688c91a78466dcf7d67e
Dattatray <dattatrayh@gmail.com>
2019-04-03 10:50:56 +0530

    Update projects_view.php

--- application/views/pages/public/projects_view.php
+++ application/views/pages/public/projects_view.php
@@ -21,6 +21,54 @@
                 <ul class="row project_listing">
                     <li class="col-md-4">
                         <div class="equal-hight clearfix">
+                            <div id="transp"><a href="<?=base_url('publicv/contact');?>" class="get-started">Submit Project</a></div>
+                            <div class="project-img-block">
+                            	<img src="../assets/banner-assets/images/projects/project-1.jpg" class="img-responsive" alt="">
+                                <div class="pro-description">
+                                	<h3>Project 1</h3>
+                                	<h4 class="location">Country name</h4>
+                                </div>
+                            </div>
+                            <div class="cnt-block">
+                                <div class="row">
+                                	<div class="col-md-12 content-flex">
+                                    	<h3>Project Acquisition Cost:</h3>
+                                		<p>$20 M USD</p>
+                                    </div>
+                                    <div class="col-md-12"><div class="divider-line"></div></div>
+                                    <div class="col-md-6 col-xs-6">
+                                    	<h3>Sector</h3>
+                                		<p>Energy</p>
+                                    </div>
+                                    <div class="col-md-6 col-xs-6">
+                                    	<h3>IRR</h3>
+                                		<p>6%</p>
+                                    </div>
+                                    <div class="col-md-6 col-xs-6">
+                                    	<h3>Term</h3>
+                                		<p>18 Years</p>
+                                    </div>
+                                    <div class="col-md-6 col-xs-6">
+                                    	<h3>Min Investment</h3>
+                                		<p>$10 K</p>
+                                    </div>
+                                    <div class="col-md-6 col-xs-6">
+                                    	<h3>Project Stage</h3>
+                                		<p>Operational</p>
+                                    </div>
+                                    <div class="col-md-6 col-xs-6">
+                                    	<h3>Net Income</h3>
+                                		<p>$2.2 M Annually</p>
+                                    </div>
+                                    <div class="col-md-12 text-center">
+                                    	<a href="<?=base_url('publicv/projects_detail');?>" class="get-started">View Project</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="col-md-4">
+                        <div class="equal-hight clearfix">
                             <div class="project-img-block">
                             	<img src="../assets/banner-assets/images/projects/gok-hepp.jpg" class="img-responsive" alt="HRBT AS GOK HEPP Acquisition Bond">
                                 <div class="pro-description">
@@ -65,60 +113,12 @@
                                 </div>
                             </div>
                         </div>
-                    </li>                    
-                    <li class="col-md-4">
-                        <div class="equal-hight clearfix">
-                            <div id="transp"><div class="under-discussion">Under Discussion</div></div>
-                            <div class="project-img-block">
-                            	<img src="../assets/banner-assets/images/projects/project-1.jpg" class="img-responsive" alt="">
-                                <div class="pro-description">
-                                	<h3>Project 2</h3>
-                                	<h4 class="location">Country name</h4>
-                                </div>
-                            </div>
-                            <div class="cnt-block">
-                                <div class="row">
-                                	<div class="col-md-12 content-flex">
-                                    	<h3>Project Acquisition Cost:</h3>
-                                		<p>$20 M USD</p>
-                                    </div>
-                                    <div class="col-md-12"><div class="divider-line"></div></div>
-                                    <div class="col-md-6 col-xs-6">
-                                    	<h3>Sector</h3>
-                                		<p>Energy</p>
-                                    </div>
-                                    <div class="col-md-6 col-xs-6">
-                                    	<h3>IRR</h3>
-                                		<p>6%</p>
-                                    </div>
-                                    <div class="col-md-6 col-xs-6">
-                                    	<h3>Term</h3>
-                                		<p>18 Years</p>
-                                    </div>
-                                    <div class="col-md-6 col-xs-6">
-                                    	<h3>Min Investment</h3>
-                                		<p>$10 K</p>
-                                    </div>
-                                    <div class="col-md-6 col-xs-6">
-                                    	<h3>Project Stage</h3>
-                                		<p>Operational</p>
-                                    </div>
-                                    <div class="col-md-6 col-xs-6">
-                                    	<h3>Net Income</h3>
-                                		<p>$2.2 M Annually</p>
-                                    </div>
-                                    <div class="col-md-12 text-center">
-                                    	<a href="<?=base_url('publicv/projects_detail');?>" class="get-started">View Project</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </li>
                     <li class="col-md-4">
                         <div class="equal-hight clearfix">
                             <div id="transp"><div class="under-discussion">Under Discussion</div></div>
                             <div class="project-img-block">
-                            	<img src="../assets/banner-assets/images/projects/project-1.jpg" class="img-responsive" alt="">
+                            	<img src="../assets/banner-assets/images/projects/project-3.jpg" class="img-responsive" alt="">
                                 <div class="pro-description">
                                 	<h3>Project 3</h3>
                                 	<h4 class="location">Country name</h4>
