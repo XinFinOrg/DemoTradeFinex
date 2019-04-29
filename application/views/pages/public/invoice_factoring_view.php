29ea7d1657bc48d555918d1892509bd4b4545c50
mansivora <mansivora36@gamail.com>
2019-04-29 14:36:58 +0530

    id added for quickbook

--- application/views/pages/public/invoice_factoring_view.php
+++ application/views/pages/public/invoice_factoring_view.php
@@ -99,7 +99,7 @@
                                                             
                                                             <div class="card-footer border-0 text-center">
                                             
-                                                                <a href="#" class="btnn btnn-primary btn-quickbook btnn-rounded" id ="" style=" background-color: #2da11c!important;
+                                                                <a href="#" class="btnn btnn-primary btn-quickbook btnn-rounded" id ="quickbooks" style=" background-color: #2da11c!important;
     border-color: #2da11c!important; color:#fff; "> Connect with QuickBooks</a>
                                                             </div>
                                                         </div>
