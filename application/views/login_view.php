bdd565f8a457f8538dffa8f828db2c1887bdc355
mansivora <mansivora36@gamail.com>
2019-05-09 17:28:04 +0530

    facebook login bug fixing

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -7,7 +7,7 @@
 						<!-- <span class="signbox"><a href="<?=base_url()?>googlelogin/login"><img src="<?=base_url()?>assets/images/google-btn.png" alt=""/></a></span> -->
 
 						<span ><a href="https://demo.tradefinex.org/sociallogin/login"><img src="<?php echo base_url() ?>assets/images/img/google-btn.png" alt=""></a></span>
-						<span ><a href="https://demo.tradefinex.org/facebooklogin/login"><img src="<?php echo base_url() ?>assets/images/img/fb-login-btn.png" alt=""></a></span>
+						<span ><a href="https://demo.tradefinex.org/facebooklogin"><img src="<?php echo base_url() ?>assets/images/img/fb-login-btn.png" alt=""></a></span>
 						
 					</div>
 				</div>
