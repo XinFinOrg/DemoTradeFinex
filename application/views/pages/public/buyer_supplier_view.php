5203a6f288c9f85dde4c0160a251411aa559e99c
mansivora <>
2019-12-19 14:26:58 +0530

    autocomplete addde

--- application/views/pages/public/buyer_supplier_view.php
+++ application/views/pages/public/buyer_supplier_view.php
@@ -124,7 +124,7 @@
 
                                 <div class="form-group">
                                     <label for="private-key">Enter Private Key <span><a href="https://howto.xinfin.org/XinFinWallet/features/" target="_blank">How to Create PrivateKey?</a></span></label>
-                                    <input type="text" class="form-control" id="private_key" name="private_key" placeholder="Enter Private Key">
+                                    <input type="text" class="form-control" id="private_key" name="private_key" autocomplete= "off"placeholder="Enter Private Key">
                                 </div>
 
                                 <div class="form-group">
