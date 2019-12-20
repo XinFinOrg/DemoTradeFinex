7c4d66c4aff4509fad54173188def54b454b9529
mansivora <>
2019-12-20 15:34:13 +0530

    financier page updated

--- application/views/pages/public/brokers_view.php
+++ application/views/pages/public/brokers_view.php
@@ -81,11 +81,11 @@
                                         <label for="currency_supported">Currency</label>
                                         <select class="form-control" id="currency_supported" name="currency_supported">
                                             <option value="" disabled="" selected="">Select Currency</option>
-                                            <option value="usd">USD</option>
-                                            <option value="gbp">GBP</option>
-                                            <option value="jpy">JPY</option>
-                                            <option value="eur">EUR</option>
-                                            <option value="xdc">XDC</option>
+                                            <option value="USD">USD</option>
+                                            <option value="GBP">GBP</option>
+                                            <option value="JPY">JPY</option>
+                                            <option value="EUR">EUR</option>
+                                            <option value="XDC">XDC</option>
                                         </select>
                                     </div>
                                     <div class="form-group col-md-6">
