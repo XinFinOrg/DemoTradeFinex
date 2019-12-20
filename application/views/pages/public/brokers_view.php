8d574222cdfddb513827aa915d9bd5f2977f4b23
mansivora <>
2019-12-20 17:24:08 +0530

    changes

--- application/views/pages/public/brokers_view.php
+++ application/views/pages/public/brokers_view.php
@@ -177,11 +177,13 @@
                                     <input type="text" class="form-control" id="privateKey" name="privateKey" autocomplete= "off"placeholder="Enter Private Key">
                                 </div>
 
-                                <div class="form-group">
-                                    <button  id = "contractdoc" name = "suppliers" type="submit" class="btn btn-blue text-uppercase" >Submit</button>
-                                </div>
-                                <div class="form-group col-md-6 col-xs-6 text-right">
+                                <div class="row">
+									<div class="form-group col-md-6 col-xs-6">
+										<button  id = "contractdoc" name = "suppliers" type="submit" class="btn btn-blue text-uppercase" >Submit</button>
+									</div>
+									<div class="form-group col-md-6 col-xs-6 text-right">
 										<a id="getDoc" onclick="hideShow()"target="_blank" class="btn btn-white btn-small"><i class="fa fa-angle-double-left"></i> Go Back</a></span>
+									</div>
 								</div>
                             </form>
                         </div>
