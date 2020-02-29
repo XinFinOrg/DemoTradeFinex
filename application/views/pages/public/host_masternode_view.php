9704ff928d02101f66792cf2c878cabc3d001c0e
Dattatray <dattatrayh@gmail.com>
2020-02-29 19:50:58 +0530

    Masternode page updated
    host_masternode_view.php page changes,
    tf-styles.css changes,
    Masternode icons created

--- application/views/pages/public/host_masternode_view.php
+++ application/views/pages/public/host_masternode_view.php
@@ -3,29 +3,27 @@
 
     <section class="tf-inner-banner">
         <div class="container">
-            <h3>Host Masternode</h3>
-            <h4>Tokens to host Masternode</h4>
+            <h3>Staking & Masternodes Services</h3>
+            <h4>Host Masternode in a few clicks</h4>
         </div>
     </section>
 	
 	
-	<!-- Statistics -->
+	<!-- Statistics 
     <section id="statistics" class="section">
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <div class="section-title text-center noBorderAfter">
                         <h2 class="mb-10">Host Masternode</h2>
-						<!--<p>&nbsp;</p>-->
                     </div>
                 </div>
             </div>
 			
-			<div class="row">
-			<div class="col-md-10 col-md-offset-1">
+			
 			<div class="row flex-row">
-						<div class="col-md-4 col-sm-4 col-xs-6">
-						<div class="tf-host-mn bgOffWhiteShadow">
+						<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
+						<div class="tf-host-mn bgOffWhiteShadow vertical-align">
 							<div class="mn-icon">
 								<img src="../assets/images/host-masternode-icons/xdc.png" />
 							</div>
@@ -35,8 +33,8 @@
 							</div>
 						</div>
 						</div>
-						<div class="col-md-4 col-sm-4 col-xs-6">
-						<div class="tf-host-mn bgOffWhiteShadow">
+						<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
+						<div class="tf-host-mn bgOffWhiteShadow vertical-align">
 							<div class="mn-icon">
 								<img src="../assets/images/host-masternode-icons/tokens.png" />
 							</div>
@@ -46,8 +44,8 @@
 							</div>
 						</div>
 						</div>
-						<div class="col-md-4 col-sm-4 col-xs-6">
-						<div class="tf-host-mn bgOffWhiteShadow">
+						<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
+						<div class="tf-host-mn bgOffWhiteShadow vertical-align">
 							<div class="mn-icon">
 								<img src="../assets/images/host-masternode-icons/dollar.png" />
 							</div>
@@ -57,19 +55,151 @@
 							</div>
 						</div>
 						</div>
+						<div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
+						<div class="tf-host-mn bgOffWhiteShadow vertical-align">
+							<div class="mn-icon">
+								<img src="../assets/images/host-masternode-icons/hosting-charges.png" />
+							</div>
+							<div class="mn-desc">
+								<p>Hosting Charges</p>
+								<select class="mb10 form-control" id="tab_selector">
+                                    <option value="Indsoft">Indsoft</option>
+                                    <option value="Amazon">Amazon</option>
+                                    <option value="Google Cloud">Google Cloud</option>
+									<option value="Azure">Azure</option>
+									<option value="Alibaba">Alibaba</option>
+                                </select>
+								<div class="factTitle">$ 800</div>
+							</div>
+						</div>
+						</div>
 						
-						<div class="col-md-12">
+						<div class="col-lg-12 col-xs-12">
                 			<div class="text-center">
                 				<a href="http://localhost/DemoTradeFinex/publicv/videos" class="btn btn-blue text-uppercase"><i class="fa fa-upload" aria-hidden="true"></i> UPLOAD</a>
             				</div>
             			</div>
 			</div>
-			</div>
-			</div>
-					
-					
         </div>
+    </section>-->
+	
+	
+	
+	
+	<!-- Statistics -->
+	<section id="statistics" class="section tf-grey-bg">
+		<div class="container">
+			<div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="section-title text-center noBorderAfter pb-0">
+                        <h2 class="mb-0">&nbsp;</h2>
+                    </div>
+                </div>
+            </div>
+			
+			<div class="row mb-30 vertical-align flex-row">
+						<div class="col-md-4">
+							<div class="start-now-block">
+								<h2 class="mb-20">Start Now</h2>
+								<p class="mb-30">Fast, Reliable and Affordable Masternode Hosting.</p>
+								<div class="upload-kyc-btn ukb-desktop">
+									<a href="http://localhost/DemoTradeFinex/publicv/videos" class="btn btn-blue text-uppercase"><i class="fa fa-upload" aria-hidden="true"></i>&nbsp; UPLOAD KYC</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-md-8">
+						<div class="row flex-row">
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<div class="tf-host-mn bgOffWhiteShadow vertical-align">
+							<div class="mn-icon">
+								<img src="../assets/images/host-masternode-icons/xdc.png" />
+							</div>
+							<div class="mn-desc">
+								<p>XDC PRICE</p>
+								<div class="factTitle">$ 0.001220</div>
+							</div>
+						</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<div class="tf-host-mn bgOffWhiteShadow vertical-align">
+							<div class="mn-icon">
+								<img src="../assets/images/host-masternode-icons/tokens.png" />
+							</div>
+							<div class="mn-desc">
+								<p>Tokens to Host Masternode</p>
+								<div class="factTitle">10M XDC</div>
+							</div>
+						</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<div class="tf-host-mn bgOffWhiteShadow vertical-align">
+							<div class="mn-icon">
+								<img src="../assets/images/host-masternode-icons/dollar.png" />
+							</div>
+							<div class="mn-desc">
+								<p>Price in US Dollar</p>
+								<div class="factTitle">$ 0.00039136</div>
+							</div>
+						</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<div class="tf-host-mn bgOffWhiteShadow vertical-align">
+							<div class="mn-icon">
+								<img src="../assets/images/host-masternode-icons/hosting-charges.png" />
+							</div>
+							<div class="mn-desc">
+								<p>Hosting Charges</p>
+								<select class="mb10 form-control" id="tab_selector">
+                                    <option value="Indsoft">Indsoft</option>
+                                    <option value="Amazon">Amazon</option>
+                                    <option value="Google Cloud">Google Cloud</option>
+									<option value="Azure">Azure</option>
+									<option value="Alibaba">Alibaba</option>
+                                </select>
+								<div class="factTitle">$ 800</div>
+							</div>
+						</div>
+						</div>
+						
+						<div class="col-lg-12 col-md-12 col-xs-12 text-center">
+						<div class="upload-kyc-btn ukb-mobile">
+									<a href="http://localhost/DemoTradeFinex/publicv/videos" class="btn btn-blue text-uppercase"><i class="fa fa-upload" aria-hidden="true"></i>&nbsp; UPLOAD KYC</a>
+								</div>
+								</div>
+								
+						</div>
+						</div>
+			</div>
+			
+			<div class="row">			
+			<div class="col-lg-6 col-md-6">
+			<div class="start-now-desc">
+			<ul>
+  <li><i class="fa"><img src="../assets/images/host-masternode-icons/wallet.png"/></i> Your coins stay in your wallet</li>
+  <li><i class="fa"><img src="../assets/images/host-masternode-icons/address.png"/></i> Service requires wallet address only</li>
+  <li><i class="fa"><img src="../assets/images/host-masternode-icons/rightcheck.png"/></i> Same as own VPS price</li>
+</ul></div>
+</div>
+
+<div class="col-lg-6 col-md-6">
+<div class="start-now-desc">
+ <ul>
+  <li><i class="fa"><img src="../assets/images/host-masternode-icons/time.png"/></i> Maximum uptime of your node</li>
+  <li><i class="fa"><img src="../assets/images/host-masternode-icons/setup.png"/></i> One minute fast setup</li>
+  <li><i class="fa"><img src="../assets/images/host-masternode-icons/payments.png"/></i> We accept PayPal and XDC</li>
+ </ul>
+</div>
+
+</div>
+
+</div>
+		</div>
     </section>
+
+
+
+
 	
 </div>
 
