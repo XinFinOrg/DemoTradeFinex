82db375eb621df79fc1a89c461b15b2b6050258d
mansivora <mansivora36@gmail.com>
2020-03-11 15:06:31 +0530

    viral page updated

---
+++ application/views/pages/public/viral_strategy.php
@@ -0,0 +1,63 @@
+<!-- Inside Page About XinFin Masternode -->
+<div class="sub_page_wraper">
+
+<section class="tf-inner-banner">
+    <div class="container">
+        <h3>Viral Strategy</h3>
+    </div>
+</section>
+
+<!-- About XinFin Masternode -->
+<section id="tf-setup-masternode" class="section">
+    <div class="container">
+    <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="section-title text-center">
+                <h3>Is Your business affected by coronavirus or external crisis affecting your business?</h3>
+                <h4>TradeFinex lets you connect with funding sources that provide easy credit facilities.</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="section-title text-center">
+                    <h2 class="mb-0">How to start?</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="orderList">
+                    <ol>
+                        <li>Start Business, Register and create identity on TradeFinex (need Business Registration document)</li>
+                        <div class="bulletList">
+                            <ul>
+                                <li>After the buyer/Supplier uploads the instrument, a tokenised certificate is generated of the instrument.</li>
+                            </ul>
+                        </div>
+                        <li>Start Request for the funding:</li>
+                        <div class="bulletList">
+                            <ul>
+                                <li>Businesses can share this certificate over LinkedIn, tag major local Bank and Non-Bank funders.</li>
+                                <li>Businesses can invite funders</li>
+                            </ul>
+                        </div>
+                        <li>Funder sees the document and starts communication for the funding.</li> 
+                    </ol>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</section>
+<!-- /. About XinFin Masternode -->
+
+</div>
+<!-- /. Inside Page About XinFin Masternode -->
+
+<?php
+	//$this->load->view('includes/block_features');	
+	$this->load->view('includes/login_modal');
+	
+?>	
