c5b2c07ffb31062a75ff91b8d8287091346d1046
Mansi Vora <mansivora36@gmail.com>
2020-06-21 01:10:58 +0530

    url fixed

--- application/config/twitter.php
+++ application/config/twitter.php
@@ -7,5 +7,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['CONSUMER_KEY']      = 'ftKzO27h7mGV4uC1NEBQqZT85';
 $config['CONSUMER_SECRET']   = 'GCEKP5su8Uk3xSy1ZIPbADKo5px4P4SPbwScms96LYg41ujulr';
-$config['OAUTH_CALLBACK']    = 'https://demo.tradefinex/login/tlogin';
+$config['OAUTH_CALLBACK']    = 'https://demo.tradefinex.org/login/tlogin';
 // $config['OAUTH_CALLBACK']    = 'http://localhost/DemoTradeFinex/login/tlogin';
