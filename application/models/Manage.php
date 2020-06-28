4eae7f38a73b225afb4f649eb888c796b3e5aecb
mansivora <mansivora36@gmail.com>
2020-06-28 20:04:25 +0530

    testing

--- application/models/Manage.php
+++ application/models/Manage.php
@@ -1046,27 +1046,14 @@
 			}
 		}
 
-		public function get_notification_setting($user_id, $user_type_ref){
+		public function get_notification_setting($user_id){
 
 			$result = array();
 
-			if($user_type_ref == 1){
 				$this->db->select('*');
-				$this->db->from('{PRE}service_provider');
-				$where = "tfsp_user_ref = '".$user_id."'";
-			}
-
-			if($user_type_ref == 2){
-				$this->db->select('*');
-				$this->db->from('{PRE}financier');
-				$where = "tff_user_ref = '".$user_id."'";
-			}
-
-			if($user_type_ref == 3){
-				$this->db->select('*');
-				$this->db->from('{PRE}beneficiary');
-				$where = "tfb_user_ref = '".$user_id."'";
-			}
+				$this->db->from('{PRE}social_user');
+				$where = "tfs_id = '".$user_id."'";
+			
 
 			$this->db->where($where);
 			$query = $this->db->get();
